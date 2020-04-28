96dd363f418201ed58dac155f585078139ceef24
guanguans <yzmguanguan@gmail.com>
2020-04-28 10:33:16 +0800

    Add testing

--- tests/JpushTest.php
+++ tests/JpushTest.php
@@ -11,7 +11,12 @@
 namespace Guanguans\Tests;
 
 use JPush\Client;
+use JPush\DevicePayload;
+use JPush\PushPayload;
+use JPush\ReportPayload;
+use JPush\SchedulePayload;
 use Yii;
+use yii\base\UnknownMethodException;
 
 class JpushTest extends TestCase
 {
@@ -27,5 +32,29 @@ class JpushTest extends TestCase
     {
         $this->assertInstanceOf(Client::class, $this->jpush->client);
         $this->assertInstanceOf(Client::class, $this->jpush->getClient());
+    }
+
+    public function testInvalidMethod()
+    {
+        $method = 'moc_method';
+        $this->expectException(UnknownMethodException::class);
+        $this->expectExceptionMessage(sprintf('Method does not exist. : %s', $method));
+        $this->jpush->$method();
+        $this->fail('Faild to assert call not exist method throw exception with invalid mehtod.');
+    }
+
+    public function testCall()
+    {
+        $this->assertInstanceOf(PushPayload::class, $this->jpush->push());
+        $this->assertInstanceOf(ReportPayload::class, $this->jpush->report());
+        $this->assertInstanceOf(DevicePayload::class, $this->jpush->device());
+        $this->assertInstanceOf(SchedulePayload::class, $this->jpush->schedule());
+
+        $this->assertSame($this->jpush->getAuthStr(), $this->jpush->client->getAuthStr());
+        $this->assertSame($this->jpush->getRetryTimes(), $this->jpush->client->getRetryTimes());
+        $this->assertSame($this->jpush->getLogFile(), $this->jpush->client->getLogFile());
+
+        $this->assertSame($this->jpush->is_group(), $this->jpush->client->is_group());
+        $this->assertSame($this->jpush->makeURL('push'), $this->jpush->client->makeURL('push'));
     }
 }
