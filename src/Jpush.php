0050c3f3d9dc1eeda1535f60acd30bfe0c365880
guanguans <yzmguanguan@gmail.com>
2020-10-23 15:28:54 +0800

    Add `symfony/options-resolver`

--- src/Jpush.php
+++ src/Jpush.php
@@ -13,6 +13,7 @@ namespace Guanguans\YiiJpush;
 use Guanguans\YiiJpush\Traits\Macroable;
 use JPush\Client;
 use JPush\Config;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Yii;
 use yii\base\Component;
 use yii\base\UnknownMethodException;
@@ -34,6 +35,8 @@ class Jpush extends Component
 
     public $zone;
 
+    protected $options = [];
+
     protected $client;
 
     /**
@@ -46,7 +49,45 @@ class Jpush extends Component
     {
         parent::init();
 
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+        $this->options = $resolver->resolve([
+            'appKey' => $this->appKey,
+            'masterSecret' => $this->masterSecret,
+            'logFile' => $this->logFile,
+            'retryTimes' => $this->retryTimes,
+            'zone' => $this->zone,
+        ]);
+
         $this->client = Yii::createObject(Client::class, [$this->appKey, $this->masterSecret, $this->logFile, $this->retryTimes, $this->zone]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'appKey' => $this->appKey,
+            'masterSecret' => $this->masterSecret,
+            'logFile' => $this->logFile,
+            'retryTimes' => $this->retryTimes,
+            'zone' => $this->zone,
+        ]);
+        $resolver->setRequired(['appKey', 'masterSecret']);
+        $resolver->setAllowedTypes('retryTimes', 'int');
+        $resolver->setAllowedTypes('appKey', 'string');
+        $resolver->setAllowedTypes('masterSecret', 'string');
+    }
+
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
     }
 
     /**
